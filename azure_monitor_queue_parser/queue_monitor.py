2bcbc92687a7bb2938de473e0184e0d06d185c24
Matt Larsen <matt.larsen@connorgp.com>
2021-01-28 17:06:54 -0700

    required __init__ params for QueueMonitor

--- azure_monitor_queue_parser/queue_monitor.py
+++ azure_monitor_queue_parser/queue_monitor.py
@@ -1,3 +1,4 @@
 class QueueMonitor:
-    def __init__(self):
-        pass
+    def __init__(self, queue_name, conn_str):
+        self.queue_name = queue_name
+        self.conn_str = conn_str
--- tests/test_queue_monitor.py
+++ tests/test_queue_monitor.py
@@ -1,6 +1,22 @@
+import pytest
+
 from azure_monitor_queue_parser import QueueMonitor
 
 
 def test_queue_monitor_class():
-    queue_monitor = QueueMonitor()
+    queue_monitor = QueueMonitor(queue_name="blah", conn_str="blah")
     assert isinstance(queue_monitor, object)
+
+
+def test_queue_monitor_init_queue_name_param():
+    # QueueMonitor() needs to take a connection string and a queue name as __init__ arguments
+    with pytest.raises(TypeError) as excinfo:
+        _ = QueueMonitor()
+    assert "queue_name" in str(excinfo.value)
+
+
+def test_queue_monitor_init_conn_str_param():
+    # QueueMonitor() needs to take a connection string and a queue name as __init__ arguments
+    with pytest.raises(TypeError) as excinfo:
+        _ = QueueMonitor()
+    assert "conn_str" in str(excinfo.value)
